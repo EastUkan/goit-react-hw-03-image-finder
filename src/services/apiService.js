1f86ebcd19e136376b0b8a16e91855887d666704
Andrii Kryvenko <andkryvenko@gmail.com>
2021-01-28 11:02:11 +0200

    rewrite apiService

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 
-const apiService = async (query, page) => {
-  const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=18963132-a5d8582da162d4c0f5ee62384&image_type=photo&orientation=horizontal&per_page=12`,
-  );
-  return data.hits;
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '20017107-2452682bd85dae6d03848b3b1';
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
+
+const apiService = async ({ q, page }) => {
+  try {
+    const { data } = await axios.get('', { params: { q, page } });
+    return data.hits;
+  } catch (error) {
+    console.log('error', { error });
+    return [];
+  }
 };
 
 export default apiService;
